aaa601130374245511fc434cf3044caa06ad5db3
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-02-18 06:45:26 +0900

    copycat

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "feature.js",
+    library: "feature",
+    libraryTarget: "var",
+  },
+};
